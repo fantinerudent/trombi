bfa4529d079b8a5d0c57bf61a4796e3efce8df8f
Fantine R <56721893+fantinerudent@users.noreply.github.com>
2020-09-28 12:47:25 +0200

    working on form and handling changes

--- src/components/Login.js
+++ src/components/Login.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 import styled from "styled-components";
 
 const StyledForm = styled.form`
@@ -9,24 +9,86 @@ const StyledForm = styled.form`
   border-radius: 20px;
   & input,
   & button {
-      background-color: lightyellow;
+    background-color: lightyellow;
     font-style: italic;
     margin-bottom: 10px;
     height: 30px;
   }
+  & label {
+    font-size: 0.2em;
+    margin-bottom: 10px;
+  }
 `;
 
 function Login() {
-    const handleSubmit = (event) => {
-        event.preventDefault();
-    };
+  const [isNameEmpty, setNameStatus] = useState(true);
+  const [name, setName] = useState("");
+  const [isCompanyEmpty, setCompanyStatus] = useState(true);
+  const [company, setCompany] = useState("");
+  const [isPasswordEmpty, setPasswordStatus] = useState(true);
+  const [password, setPassword] = useState("");
+  const handleSubmit = (event) => {
+    event.preventDefault();
+  };
+
+  const handleChangeName = (e) => {
+    if (e.currentTarget.value.length) {
+      setNameStatus(false);
+      return setName(e.currentTarget.value);
+    }
+    setNameStatus(true);
+  };
+  const handleChangeCompany = (e) => {
+    if (e.currentTarget.value.length) {
+      setCompanyStatus(false);
+      return setCompany(e.currentTarget.value);
+    }
+    setCompanyStatus(true);
+  };
+  const handleChangePassword = (e) => {
+    if (e.currentTarget.value.length) {
+      setPasswordStatus(false);
+      return setPassword(e.currentTarget.value);
+    }
+    setPasswordStatus(true);
+  };
 
   return (
     <>
       <StyledForm id="login" onSubmit={(event) => handleSubmit(event)}>
-        <input type="text" name="name" placeholder="name" />
-        <input type="password" name="password" placeholder="password" />
-        <button type="submit" > send </button>
+        <label> Enter your name or the company's name</label>
+        <input
+          onChange={(e) => {
+            handleChangeName(e);
+          }}
+          type="text"
+          name="name"
+          placeholder="name"
+          autoComplete="off"
+        />
+        <input
+          onChange={(e) => {
+            handleChangeCompany(e);
+          }}
+          type="text"
+          name="company"
+          placeholder="company"
+          autoComplete="off"
+        />
+        <input
+          onChange={(e) => {
+            handleChangePassword(e);
+          }}
+          type="password"
+          name="password"
+          placeholder="password"
+        />
+        <button
+          type="submit"
+          disabled={(isNameEmpty && isCompanyEmpty) || isPasswordEmpty}
+        >
+          send
+        </button>
       </StyledForm>
     </>
   );
